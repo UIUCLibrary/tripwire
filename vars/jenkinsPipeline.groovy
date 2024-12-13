2e3eaf5fd7b4ec119e76a63401961ab8eb733bb4
Henry Borchers <henryborchers@yahoo.com>
2024-12-13 13:28:58 -0600

    stuff

--- vars/jenkinsPipeline.groovy
+++ vars/jenkinsPipeline.groovy
@@ -64,101 +64,101 @@ def call(){
             booleanParam(name: 'DEPLOY_STANDALONE_PACKAGERS', defaultValue: false, description: 'Deploy standalone packages')
         }
         stages {
-            stage('Building and Resting'){
-                stages{
-                    stage('Build and Test'){
-                        environment{
-                            PIP_CACHE_DIR='/tmp/pipcache'
-                            UV_INDEX_STRATEGY='unsafe-best-match'
-                            UV_TOOL_DIR='/tmp/uvtools'
-                            UV_PYTHON_INSTALL_DIR='/tmp/uvpython'
-                            UV_CACHE_DIR='/tmp/uvcache'
-                        }
-                        agent {
-                            docker{
-                                image 'python'
-                                label 'docker && linux'
-                                args "--mount source=uv_python_install_dir,target=/tmp/uvpython"
-                            }
-
-                        }
-                        stages{
-                            stage('Setup CI Environment'){
-                                steps{
-                                    sh(
-                                        label: 'Create virtual environment with packaging in development mode',
-                                        script: '''python3 -m venv --clear bootstrap_uv
-                                                   trap "rm -rf bootstrap_uv" EXIT
-                                                   bootstrap_uv/bin/pip install --disable-pip-version-check uv
-                                                   rm -rf venv
-                                                   bootstrap_uv/bin/uv venv venv
-                                                   . ./venv/bin/activate
-                                                   bootstrap_uv/bin/uv pip install uv
-                                                   rm -rf bootstrap_uv
-                                                   uv pip install -r requirements-dev.txt -e .
-                                                   '''
-                                   )
-                                }
-                            }
-                            stage('Run Tests'){
-                                when{
-                                    equals expected: true, actual: params.RUN_CHECKS
-                                    beforeAgent true
-                                }
-                                parallel {
-                                    stage('Run PyTest Unit Tests'){
-                                        steps{
-                                            catchError(buildResult: 'UNSTABLE', message: 'Did not pass all pytest tests', stageResult: 'UNSTABLE') {
-                                                sh(
-                                                    script: '''. ./venv/bin/activate
-                                                               PYTHONFAULTHANDLER=1 coverage run --parallel-mode --source=avtool -m pytest --junitxml=./reports/tests/pytest/pytest-junit.xml --capture=no
-                                                           '''
-                                                )
-                                            }
-                                        }
-                                        post {
-                                            always {
-                                                junit(allowEmptyResults: true, testResults: 'reports/tests/pytest/pytest-junit.xml')
-                                            }
-                                        }
-                                    }
-                                    stage('Task Scanner'){
-                                        steps{
-                                            recordIssues(tools: [taskScanner(highTags: 'FIXME', includePattern: 'speedwagon/**/*.py', normalTags: 'TODO')])
-                                        }
-                                    }
-                                }
-                                post{
-                                    always{
-                                        sh '''. ./venv/bin/activate
-                                              coverage combine && coverage xml -o reports/coverage.xml && coverage html -d reports/coverage
-                                           '''
-                                        recordCoverage(tools: [[parser: 'COBERTURA', pattern: 'reports/coverage.xml']])
-                                    }
-                                }
-                            }
-                        }
-                        post{
-                            cleanup{
-                                cleanWs(
-                                    deleteDirs: true,
-                                    patterns: [
-                                        [pattern: '.pytest_cache/', type: 'INCLUDE'],
-                                        [pattern: '*.egg-info', type: 'INCLUDE'],
-                                        [pattern: 'coverage/', type: 'INCLUDE'],
-                                        [pattern: '**/.coverage', type: 'INCLUDE'],
-                                        [pattern: 'venv/', type: 'INCLUDE'],
-                                        [pattern: 'logs/', type: 'INCLUDE'],
-                                        [pattern: 'reports/', type: 'INCLUDE'],
-                                        [pattern: 'coverage-sources.zip', type: 'INCLUDE'],
-                                        [pattern: '**/__pycache__/', type: 'INCLUDE'],
-                                    ]
-                                )
-                            }
-                        }
-                    }
-                }
-            }
+//             stage('Building and Resting'){
+//                 stages{
+//                     stage('Build and Test'){
+//                         environment{
+//                             PIP_CACHE_DIR='/tmp/pipcache'
+//                             UV_INDEX_STRATEGY='unsafe-best-match'
+//                             UV_TOOL_DIR='/tmp/uvtools'
+//                             UV_PYTHON_INSTALL_DIR='/tmp/uvpython'
+//                             UV_CACHE_DIR='/tmp/uvcache'
+//                         }
+//                         agent {
+//                             docker{
+//                                 image 'python'
+//                                 label 'docker && linux'
+//                                 args '--mount source=uv_python_install_dir,target=/tmp/uvpython'
+//                             }
+//
+//                         }
+//                         stages{
+//                             stage('Setup CI Environment'){
+//                                 steps{
+//                                     sh(
+//                                         label: 'Create virtual environment with packaging in development mode',
+//                                         script: '''python3 -m venv --clear bootstrap_uv
+//                                                    trap "rm -rf bootstrap_uv" EXIT
+//                                                    bootstrap_uv/bin/pip install --disable-pip-version-check uv
+//                                                    rm -rf venv
+//                                                    bootstrap_uv/bin/uv venv venv
+//                                                    . ./venv/bin/activate
+//                                                    bootstrap_uv/bin/uv pip install uv
+//                                                    rm -rf bootstrap_uv
+//                                                    uv pip install -r requirements-dev.txt -e .
+//                                                    '''
+//                                    )
+//                                 }
+//                             }
+//                             stage('Run Tests'){
+//                                 when{
+//                                     equals expected: true, actual: params.RUN_CHECKS
+//                                     beforeAgent true
+//                                 }
+//                                 parallel {
+//                                     stage('Run PyTest Unit Tests'){
+//                                         steps{
+//                                             catchError(buildResult: 'UNSTABLE', message: 'Did not pass all pytest tests', stageResult: 'UNSTABLE') {
+//                                                 sh(
+//                                                     script: '''. ./venv/bin/activate
+//                                                                PYTHONFAULTHANDLER=1 coverage run --parallel-mode --source=avtool -m pytest --junitxml=./reports/tests/pytest/pytest-junit.xml --capture=no
+//                                                            '''
+//                                                 )
+//                                             }
+//                                         }
+//                                         post {
+//                                             always {
+//                                                 junit(allowEmptyResults: true, testResults: 'reports/tests/pytest/pytest-junit.xml')
+//                                             }
+//                                         }
+//                                     }
+//                                     stage('Task Scanner'){
+//                                         steps{
+//                                             recordIssues(tools: [taskScanner(highTags: 'FIXME', includePattern: 'speedwagon/**/*.py', normalTags: 'TODO')])
+//                                         }
+//                                     }
+//                                 }
+//                                 post{
+//                                     always{
+//                                         sh '''. ./venv/bin/activate
+//                                               coverage combine && coverage xml -o reports/coverage.xml && coverage html -d reports/coverage
+//                                            '''
+//                                         recordCoverage(tools: [[parser: 'COBERTURA', pattern: 'reports/coverage.xml']])
+//                                     }
+//                                 }
+//                             }
+//                         }
+//                         post{
+//                             cleanup{
+//                                 cleanWs(
+//                                     deleteDirs: true,
+//                                     patterns: [
+//                                         [pattern: '.pytest_cache/', type: 'INCLUDE'],
+//                                         [pattern: '*.egg-info', type: 'INCLUDE'],
+//                                         [pattern: 'coverage/', type: 'INCLUDE'],
+//                                         [pattern: '**/.coverage', type: 'INCLUDE'],
+//                                         [pattern: 'venv/', type: 'INCLUDE'],
+//                                         [pattern: 'logs/', type: 'INCLUDE'],
+//                                         [pattern: 'reports/', type: 'INCLUDE'],
+//                                         [pattern: 'coverage-sources.zip', type: 'INCLUDE'],
+//                                         [pattern: '**/__pycache__/', type: 'INCLUDE'],
+//                                     ]
+//                                 )
+//                             }
+//                         }
+//                     }
+//                 }
+//             }
             stage('Package'){
                 stages{
                     stage('Python Packages'){
@@ -176,13 +176,13 @@ def call(){
                                     docker {
                                         image 'python'
                                         label 'docker && linux'
-                                        args '--mount source=python-tmp-galatea,target=/tmp'
+                                        args '--mount source=uv_python_install_dir,target=/tmp/uvpython'
                                     }
                                 }
                                 steps{
                                     sh(
                                         label: 'Package',
-                                        script: '''python3 -m venv
+                                        script: '''python3 -m venv venv
                                                    trap "rm -rf venv" EXIT
                                                    venv/bin/pip install --disable-pip-version-check uv
                                                    venv/bin/uv build
@@ -237,14 +237,10 @@ def call(){
                                             }
                                         }
                                     }
-                                    when{
-                                        equals expected: true, actual: params.TEST_PACKAGES
-                                    }
                                     stages {
                                         stage('Test Package in container') {
                                             environment{
                                                 PIP_CACHE_DIR="${isUnix() ? '/tmp/pipcache': 'C:\\Users\\ContainerUser\\Documents\\pipcache'}"
-                                                UV_INDEX_STRATEGY='unsafe-best-match'
                                                 UV_TOOL_DIR="${isUnix() ? '/tmp/uvtools': 'C:\\Users\\ContainerUser\\Documents\\uvtools'}"
                                                 UV_PYTHON_INSTALL_DIR="${isUnix() ? '/tmp/uvpython': 'C:\\Users\\ContainerUser\\Documents\\uvpython'}"
                                                 UV_CACHE_DIR="${isUnix() ? '/tmp/uvcache': 'C:\\Users\\ContainerUser\\Documents\\uvcache'}"
@@ -259,6 +255,7 @@ def call(){
                                                 docker {
                                                     image 'python'
                                                     label "${OS} && ${ARCHITECTURE} && docker"
+                                                    args "--mount source=uv_python_install_dir,target=${env.UV_PYTHON_INSTALL_DIR}"
                                                 }
                                             }
                                             steps {
@@ -309,9 +306,8 @@ def call(){
                                                         sh(
                                                             label: 'Testing with tox',
                                                             script: """python3 -m venv venv
-                                                                       . ./venv/bin/activate
-                                                                       pip install uv
-                                                                       UV_INDEX_STRATEGY=unsafe-best-match uvx --with tox-uv tox --installpkg ${findFiles(glob: PACKAGE_TYPE == 'wheel' ? 'dist/*.whl' : 'dist/*.tar.gz')[0].path} -e py${PYTHON_VERSION.replace('.', '')}
+                                                                       ./venv/bin/pip install --disable-pip-version-check uv
+                                                                       ./venv/bin/uvx --with tox-uv tox --installpkg ${findFiles(glob: PACKAGE_TYPE == 'wheel' ? 'dist/*.whl' : 'dist/*.tar.gz')[0].path} -e py${PYTHON_VERSION.replace('.', '')}
                                                                     """
                                                         )
                                                 }
@@ -419,10 +415,14 @@ def call(){
                                 }
                             }
                             stage('Windows Application'){
+                                environment{
+                                    UV_PYTHON_INSTALL_DIR='C:\\Users\\ContainerUser\\Documents\\uvpython'
+                                }
                                 agent{
                                     docker{
                                         image 'python'
                                         label 'windows && docker && x86_64'
+                                        args '--mount source=uv_python_install_dir,target=C:\\Users\\ContainerUser\\Documents\\uvpython'
                                     }
                                 }
                                 when{
@@ -477,6 +477,7 @@ def call(){
                             docker{
                                 image 'python'
                                 label 'docker && linux'
+                                args '--mount source=uv_python_install_dir,target=/tmp/uvpython'
                             }
                         }
                         when{
