c01ad8ca748677dc007783c59ac85917e78dc9fa
Henry Borchers <henryborchers@yahoo.com>
2024-12-13 12:21:05 -0600

    ci: support testing of python 3.11

--- vars/jenkinsPipeline.groovy
+++ vars/jenkinsPipeline.groovy
@@ -209,7 +209,7 @@ def call(){
                                     axes {
                                         axis {
                                             name 'PYTHON_VERSION'
-                                            values '3.12','3.13'
+                                            values '3.11', '3.12','3.13'
                                         }
                                         axis {
                                             name 'OS'
