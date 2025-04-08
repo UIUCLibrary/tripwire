3bf5d9480429f7d9b6c6fdb541e85eacc38437d7
Henry Borchers <henryborchers@yahoo.com>
2025-04-08 11:38:44 -0500

    move standalone bootstrap into own file

--- contrib/bootstrap_standalone.py
+++ contrib/bootstrap_standalone.py
@@ -2,10 +2,11 @@
 
 This is only used for freezing the application using PyInstaller.
 """
+
 import multiprocessing
 
 multiprocessing.freeze_support()
-from uiucprescon.tripwire.main import main
+from uiucprescon.tripwire.main import main  # noqa: E402
 
 if __name__ == "__main__":
     main()
